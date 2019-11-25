2c2f9923d32a883a440afaf5c3d4923daa945654
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-11-25 11:47:08 -0300

    Pagina home

--- ScrumcookiesApp/src/app/app.module.ts
+++ ScrumcookiesApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLoginComponent } from './pages/app-login/app-login.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppLoginComponent
+    AppLoginComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
