c6cda7c550f270ccf818c41df7405c40e4869a91
Técnico em Informática 2018.2 <39121742018.2@senacrjedu.local>
2019-12-10 11:37:10 -0300

    Tela profissionais

--- Spybeauty/src/app/pages/add-profissional/add-profissional.component.ts
+++ Spybeauty/src/app/pages/add-profissional/add-profissional.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfissionalServiceService } from '../../serv/profissional-service.service';
 import { Router } from '@angular/router';
+import { Profissional } from '../../modelo/profissional';
+//import Swal from 'sweetalert2'
+
+const Swal = require('sweetalert2');
 
 @Component({
   selector: 'app-add-profissional',
@@ -8,7 +12,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-profissional.component.css']
 })
 export class AddProfissionalComponent implements OnInit {
-
+protected profissional: Profissional = new Profissional;
   constructor(
     protected profService: ProfissionalServiceService,
     protected router: Router
@@ -19,12 +23,21 @@ export class AddProfissionalComponent implements OnInit {
 
   }
   onsubmit(form) {
-    this.profService.save(form).then(
+    this.profService.save(this.profissional).then(
       res => {
         //this.resetFields();
+       this.alertmessage("Sucesso", "Cadastrado com sucesso");
         this.router.navigate(['/home'])
       }
     )
+  }
+
+  alertmessage(title, text){
+    Swal.fire({
+      title: title,
+      text: text,
+      confirmButtonText: 'OK'
+    })
   }
 
 }
