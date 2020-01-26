1f844dac2c750df71c8802c1e7e839e4ee296d25
Andre Luis Silva <andrels_spfc@yahoo.com.br>
2020-01-26 00:19:43 -0300

    Apagado

--- Spybeauty/src/app/modelo/cliente.ts
+++
@@ -1,9 +0,0 @@
-import {Endereco} from 'src/app/modelo/endereco'
-export class Cliente {
-    nome: string;
-    sobrenome: string;
-    email: string;
-    senha: string;
-    cpf: string;
-    telefone: string;
-}
